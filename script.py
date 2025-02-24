b3a4c1c72a334c502e19c4242b349139a3aebaec
Minyu Deng <dengmy0318_xm@163.com>
2025-02-24 11:22:41 -0500

    feat: Update script.py to get most read page

--- script.py
+++ script.py
@@ -29,8 +29,8 @@ def scrape_data_point():
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        target_element = soup.find("a", class_="frontpage-link")
-        data_point = "" if target_element is None else target_element.text
+        target_element = soup.select_one("div.most-read li:first-of-type a")
+        data_point = "" if target_element is None else target_element.text.strip()
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
 
